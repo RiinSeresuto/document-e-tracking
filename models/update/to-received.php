26321f90320718cb217bed0f4341c746371011b6
Lester <renebaribal@gmail.com>
2023-04-27 21:05:44 +0800

    fixed received status

--- models/update/to-received.php
+++ models/update/to-received.php
@@ -14,7 +14,7 @@ $new_log_id = makeid();
 
 $query_to_received = "UPDATE `document` SET `status` = 'received', `dateUpdated` = '$timestamp', `isSent` = 1, `isReceived` = 1, `isEndorsed` = 0, `isReleased` = 0, `isReturned` = 0, `isSigned` = 0, `isApproved` = 0 WHERE `id` = '$document_id'";
 $query_new_notification = "INSERT INTO `notification`(`id`, `sender`, `receiver`, `message`, `docID`, `class`) VALUES ('$new_notification_id', '$sender_id', '$receiver_id', '$message', '$document_id', '$class')";
-$query_new_log = "INSERT INTO `log`(`id`, `documentID`, `date`, `status`, `updatedBy`) VALUES ('$new_log_id','$document_id','$timestamp','received','$receiver_id')";
+$query_new_log = "INSERT INTO `log`(`id`, `documentID`, `date`, `status`, `updatedBy`) VALUES ('$new_log_id','$document_id','$timestamp','received','$sender_id')";
 
 if(mysqli_query($db, $query_to_received)){
     if(mysqli_query($db, $query_new_notification)){
